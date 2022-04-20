865c11f6edb26fe1373384107c46a120a2d59a71
zhengyanchuang@pdmi.cn <zhengyanchuang@pdmi.cn>
2022-04-20 09:58:07 +0800

    每天提交一点点

--- leetcode/src/main/java/leetcode/editor/cn/SearchInsertPosition_35.java
+++ leetcode/src/main/java/leetcode/editor/cn/SearchInsertPosition_35.java
@@ -42,12 +42,28 @@ package leetcode.editor.cn;
 public class SearchInsertPosition_35{
     public static void main(String[] args) {
         Solution solution = new SearchInsertPosition_35().new Solution();
-        int i = solution.searchInsert(new int[]{1,2,3,4,5,6,7,8,9,10}, 2);
+        int i = solution.searchInsert(new int[]{1,3,4,5,6,7,8,9,10}, 100);
         System.out.println(i);
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int searchInsert(int[] nums, int target){
+        int left = 0,right = nums.length;
+        while (left<=right){
+            int mid = (left + right) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[mid]<target){
+                left  = mid + 1;
+            }
+            if(nums[mid]>target){
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+    public int searchInsert2(int[] nums, int target){
         for(int i =0,k = i+1;i<nums.length;i++,++k){
             if(nums[i] == target){
                 return i;
